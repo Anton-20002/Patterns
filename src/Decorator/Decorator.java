b84ff32b0f0eb28dd7c8c6b5e3809d124cc3097a
Anton-20002 <austere@list.ru>
2019-01-15 18:21:15 +0300

    Commit bug fix1_1

--- src/Decorator/Decorator.java
+++ src/Decorator/Decorator.java
@@ -3,11 +3,11 @@ package Decorator;
 abstract class Decorator implements Element{
   private Element element;
 
-  Decorator1(Element element) {
+  Decorator(Element element) {
     this.element = element;
   }
 
-  abstract void afterDraw1();
+  abstract void afterDraw();
 @Override
   public void draw() {
     element.draw();
